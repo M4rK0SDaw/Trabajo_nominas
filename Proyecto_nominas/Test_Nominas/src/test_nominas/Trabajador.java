62e225d15eb2bcc28a025bc90e89546aeefafdcf
Efren888 <95874027+Efren888@users.noreply.github.com>
2021-12-14 10:16:13 +0100

    Update Trabajador.java

--- Proyecto_nominas/Test_Nominas/src/test_nominas/Trabajador.java
+++ Proyecto_nominas/Test_Nominas/src/test_nominas/Trabajador.java
@@ -10,5 +10,55 @@ package test_nominas;
  * @author Marek
  */
 public class Trabajador {
-    
+    private String Trabajador, Nif, SeguridadSocial, GProfesional, GCotizacion;
+
+    public String setTrabajador(String a) {
+        Trabajador = a;
+        return Trabajador;
+    }
+
+    public String getTrabajador() {
+        System.out.print("Trabajador:");
+        return Trabajador;
+    }
+
+    public String setNif(String b) {
+        Nif = b;
+        return Nif;
+    }
+
+    public String getNif() {
+        System.out.print("Nif:");
+        return Nif;
+    }
+
+    public String setSeguridadSocial(String c) {
+        SeguridadSocial = c;
+        return SeguridadSocial;
+    }
+
+    public String getSeguridadSocial() {
+        System.out.print("SeguridadSocial:");
+        return SeguridadSocial;
+    }
+
+    public String setGProfesional(String d) {
+        GProfesional = d;
+        return Trabajador;
+    }
+
+    public String getGProfesional() {
+        System.out.print("GProfesional:");
+        return GProfesional;
+    }
+
+    public String setGCotizacion(String e) {
+        GCotizacion = e;
+        return GCotizacion;
+    }
+
+    public String getGCotizacion() {
+        System.out.print("GCotizacion:");
+        return GCotizacion;
+    }
 }
