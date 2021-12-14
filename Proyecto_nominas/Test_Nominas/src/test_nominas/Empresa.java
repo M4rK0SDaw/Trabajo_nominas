e713b692188d99c3eda73fb7e7b38c6616093751
Efren888 <95874027+Efren888@users.noreply.github.com>
2021-12-14 10:14:41 +0100

    Update Empresa.java

--- Proyecto_nominas/Test_Nominas/src/test_nominas/Empresa.java
+++ Proyecto_nominas/Test_Nominas/src/test_nominas/Empresa.java
@@ -10,5 +10,45 @@ package test_nominas;
  * @author Marek
  */
 public class Empresa {
-    
+    private String Empresa, Domicilio, CIF, CCC;
+
+    public String setEmpresa(String a) {
+        Empresa = a;
+        return Empresa;
+    }
+
+    public String getEmpresa() {
+        System.out.print("Empresa:");
+        return Empresa;
+    }
+
+    public String setDomicilio(String b) {
+        Domicilio = b;
+        return Domicilio;
+    }
+
+    public String getDomicilio() {
+        System.out.print("Domicilio:");
+        return Domicilio;
+    }
+
+    public String setCIF(String c) {
+        CIF = c;
+        return CIF;
+    }
+
+    public String getCIF() {
+        System.out.print("CIF:");
+        return CIF;
+    }
+
+    public String setCCC(String d) {
+        CCC = d;
+        return CCC;
+    }
+
+    public String getCCC() {
+        System.out.print("CCC:");
+        return CCC;
+    }
 }
