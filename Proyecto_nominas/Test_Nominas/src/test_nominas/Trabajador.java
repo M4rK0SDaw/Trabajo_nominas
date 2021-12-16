c950113d4dbeacb87bbcd881222f8205aede4fd2
Efren888 <95874027+Efren888@users.noreply.github.com>
2021-12-16 13:02:55 +0100

    Update Trabajador.java

--- Proyecto_nominas/Test_Nominas/src/test_nominas/Trabajador.java
+++ Proyecto_nominas/Test_Nominas/src/test_nominas/Trabajador.java
@@ -10,55 +10,45 @@ package test_nominas;
  * @author Marek
  */
 public class Trabajador {
-    private String Trabajador, Nif, SeguridadSocial, GProfesional, GCotizacion;
+   private String Trabajador, Nif, SeguridadSocial, GProfesional, GCotizacion;
 
-    public String setTrabajador(String a) {
+    public void setTrabajador(String a) {
         Trabajador = a;
-        return Trabajador;
     }
 
     public String getTrabajador() {
-        System.out.print("Trabajador:");
         return Trabajador;
     }
 
-    public String setNif(String b) {
+    public void setNif(String b) {
         Nif = b;
-        return Nif;
     }
 
     public String getNif() {
-        System.out.print("Nif:");
         return Nif;
     }
 
-    public String setSeguridadSocial(String c) {
+    public void setSeguridadSocial(String c) {
         SeguridadSocial = c;
-        return SeguridadSocial;
     }
 
     public String getSeguridadSocial() {
-        System.out.print("SeguridadSocial:");
         return SeguridadSocial;
     }
 
-    public String setGProfesional(String d) {
+    public void setGProfesional(String d) {
         GProfesional = d;
-        return Trabajador;
     }
 
     public String getGProfesional() {
-        System.out.print("GProfesional:");
         return GProfesional;
     }
 
-    public String setGCotizacion(String e) {
+    public void setGCotizacion(String e) {
         GCotizacion = e;
-        return GCotizacion;
     }
 
     public String getGCotizacion() {
-        System.out.print("GCotizacion:");
         return GCotizacion;
     }
 }
