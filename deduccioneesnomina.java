87a14dbd57801cae678e9dd63b38c81d6c2a082c
Pablo0307 <73311295+Pablo0307@users.noreply.github.com>
2021-12-16 12:39:14 +0100

    Add files via upload

---
+++ deduccioneesnomina.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deducciones;
+
+/**
+ *
+ * @author pablo
+ */
+public class deduccioneesnomina {
+  private double fp = 0.10;
+  private double cc  = 4.70;
+  private double desempleo = 1.55;
+  private double desempleodd = 1.60;
+  private double irpf = 2.00;
+  double totaldeducciones = 0;
+  public double fpcalc(){
+    double fpcalc = fp * bccp * 100; 
+    return fpcalc;   
+  }
+  public double cccalc(){
+      double cccalc = cc * bccc * 100;
+      return cccalc;
+  }
+  public double desempleocalc(){
+      double desempleocalc = desempleo * bccp * 100;
+      return desempleocalc;
+  }
+  public double desempleoddcalc(){
+      double desempleoddcalc = desempleodd * bccp * 100;
+      return desempleoddcalc;
+  }
+  public double irpfcalc(){
+      double irpfcalc = irpf *  * 100;
+      return irpfcalc;
+  }
+  public double sumatoriodeducciones(){
+     double sumatoriodeducciones = irpfcalc + desempleocalc + desempleoddcalc + cccalc + fpcalc;
+     return sumatoriodeducciones;
+  }
+}
